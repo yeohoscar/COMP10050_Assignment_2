c8062925aeb4ff5b5dd1c044ebce560f2ae2e350
Oscar Yeoh <oscar.yeoh@ucdconnect.ie>
2021-04-26 18:19:57 +0100

    Mockup of function to write results to file

--- main.c
+++ main.c
@@ -9,3 +9,18 @@ int main(void) {
     runGame();
     return 0;
 }
+
+void writeResults() {
+    FILE *fp;
+
+    if ((fp = fopen("othello-results.txt", "a+")) == NULL) {
+        printf("Error opening file.");
+        exit(0);
+    } else {
+        time_t t;
+        time(&t);
+
+        fprintf(fp, "%s Winner: %s Score: %s (%c) %d:%d %s (%c)", ctime(&t), !winner!, player1.name, player1.colour, player1.score, player2.score, player2.name, player2.colour);
+        fclose(fp);
+    }
+}
